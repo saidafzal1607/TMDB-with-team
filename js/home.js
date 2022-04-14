7781e7f951a1ab868570569f78ef6994509bb8d5
OrziqulUktamov <orziuktam@mail.ru>
2022-04-14 14:25:57 +0500

    profile added

--- js/home.js
+++ js/home.js
@@ -93,3 +93,7 @@ export function displayPopularTvMovies(data) {
     // popularTvMovies.appendChild(movieEl);
   });
 }
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
--- js/router.js
+++ js/router.js
@@ -15,3 +15,5 @@ document.addEventListener("DOMContentLoaded", function (e) {
     console.log("Bu person page");
   }
 });
+
+

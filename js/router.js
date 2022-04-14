e693b03f998425709dba84066df2b07cf03514e2
Sanjar Abduraimov <sanjarbekweb@gmail.com>
2022-04-14 15:56:40 +0500

    get movie detail added

--- js/router.js
+++ js/router.js
@@ -1,9 +1,7 @@
 import * as home from "./home.js";
 import * as card from "./card.js";
+import { displayMovie, getMovie } from "./movie.js";
 let { circleBtn, cardList, body, switchOn } = card;
-
-
- 
 
 document.addEventListener("DOMContentLoaded", function (e) {
   // switchOn()
@@ -12,6 +10,14 @@ document.addEventListener("DOMContentLoaded", function (e) {
       .getPopularMovies()
       .then((data) => {
         home.displayPopularMovies(data);
+        const cardList = document.querySelectorAll(".card");
+        cardList.forEach((card) => {
+          card.addEventListener("click", (e) => {
+            const id = card.dataset.id;
+            history.pushState({ id }, null, `/detailsmovie.html`);
+            location.reload();
+          });
+        });
       })
       .catch((err) => {
         console.log(err);
@@ -20,5 +26,17 @@ document.addEventListener("DOMContentLoaded", function (e) {
   if (location.pathname === "/person.html" || location.pathname === "/person") {
     console.log("Bu person page");
   }
+  if (
+    location.pathname === "/detailsmovie.html" ||
+    location.pathname === "/movie"
+  ) {
+    // let searchParams = new URLSearchParams(location.search);
+    // searchParams.get("id")
+    const id = history.state.id;
+    console.log(history.state);
+    getMovie(id).then((data) => {
+      displayMovie(data);
+      console.log(data);
+    });
+  }
 });
-

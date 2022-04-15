18c2d223304b46ff041d83014cee1acc71dcec7c
islom.elyorivich <islomelyorivich@gmail.com>
2022-04-15 12:59:40 +0500

    changed topmovei.js to favmovie.js

--- js/favmovies.js
+++ js/favmovies.js
@@ -1,5 +1,6 @@
 import configs from "../config.js";
-import moment from "moment/dist/moment";
+import moment from "../node_modules/moment/dist/moment.js";
+
 const { API_KEY, BASE_URL, DEFAULT_IMG_URL, BASE_IMG_URL } = configs;
 
 export async function getTopMovies(page = 1) {

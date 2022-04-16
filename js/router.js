df065e7dc3b3e2b0e5e5c1baf7595b3ba4ab99a4
Asror Qayumov <qayumovasror2003@gmail.com>
2022-04-16 16:14:53 +0500

    nothing updated

--- js/router.js
+++ js/router.js
@@ -1,111 +1,17 @@
 import * as home from "./home.js";
-import * as card from "./card.js";
-import { displayMovie, getMovie } from "./movie.js";
-import {displayFavMovie, getFavMovie} from "./favmovies.js";
-import * as favpersons from "./favperson.js";
-let { switchOn } = card;
-// switchOn(document);
-
-
-document.addEventListener("DOMContentLoaded", function (e){
-  if (location.pathname === "/favperson.html" || location.pathname === "/"){
-    // console.log(data);
-    favpersons
-    .getFavPerson()
-    .then((data)=>{
-      favpersons.displayFavPerson(data)
-      console.log(data);
-    })
-    .catch((err) =>{
-      console.log(err);
-    })
-  }
-});
-
 
 document.addEventListener("DOMContentLoaded", function (e) {
   if (location.pathname === "/index.html" || location.pathname === "/") {
     home
-      .getPopularMovies()
+      .getPopularTvMovies()
       .then((data) => {
-        home.displayPopularMovies(data);
-        const cardList = document.querySelectorAll(".card");
-        cardList.forEach((card) => {
-          card.addEventListener("click", (e) => {
-            const id = card.dataset.id;
-            history.pushState({ id }, null, `/movie.html`);
-            location.reload();
-          });
-        });
+        home.displayPopularTvMovies(data);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  // if (location.pathname === "/person.html" || location.pathname === "/person") {
-  //   console.log("Bu person pagee");
-  // }
-  if (
-    location.pathname === "/movie.html" ||
-    location.pathname === "/movie"
-  ) {
-    // let searchParams = new URLSearchParams(location.search);
-    // searchParams.get("id")
-    const id = history.state.id;
-    console.log(history.state);
-    getMovie(id).then((data) => {
-      displayMovie(data);
-      console.log(data);
-    });
+  if (location.pathname === "/person.html" || location.pathname === "/person") {
+    console.log("Bu person page");
   }
 });
-
-import * as topmovies from "./favmovies.js";
-
-document.addEventListener("DOMContentLoaded", function (e) {
-  // if (location.pathname === "/favmovie.html" || location.pathname === "/") {
-  //   topmovies
-  //     topmovies
-  //     .then((data) => {
-  //       topmovies.displayTopMovies(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }
-
-  // switchOn()
-  if (location.pathname === "/favmovie.html" || location.pathname === "/") {
-    topmovies
-      .getTopMovies()
-      .then((data) => {
-        topmovies.displayTopMovies(data);
-        const cardList = document.querySelectorAll(".card");
-        cardList.forEach((card) => {
-          card.addEventListener("click", (e) => {
-            const id = card.dataset.id;
-            history.pushState({ id }, null, `/detailsmovie.html`);
-            location.reload();
-          });
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  
-  if (
-    location.pathname === "/detailsmovie.html" ||
-    location.pathname === "/movie"
-  ) {
-    // let searchParams = new URLSearchParams(location.search);
-    // searchParams.get("id")
-    const id = history.state.id;
-    console.log(history.state);
-    getFavMovie(id).then((data) => {
-      displayFavMovie(data);
-      console.log(data);
-    });
-  }
-});
-

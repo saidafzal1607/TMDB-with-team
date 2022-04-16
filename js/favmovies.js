7d691ec87fd5f86878db48b211d02adea2877ebf
islom.elyorivich <islomelyorivich@gmail.com>
2022-04-16 14:02:36 +0500

    added samo js code in fav/person-movie

--- js/favmovies.js
+++ js/favmovies.js
@@ -83,16 +83,129 @@ export function displayTopMovies(data) {
       </div>
     `;
     topMovies.innerHTML = html;
-    // const movieEl = document.createElement("div");
-    // movieEl.classList.add("movie");
-    // movieEl.innerHTML = `
-    //   <img src="${poster}" alt="${title}" />
-    //   <div class="movie-info">
-    //     <h2>${title}</h2>
-    //     <p>${vote_average}</p>
-    //     <button class="btn" data-movie-id="${id}">More Info</button>
-    //   </div>
-    // `;
-    // popularTvMovies.appendChild(movieEl);
+   
   });
 }
+
+
+
+export async function getFavMovie(movie_id) {
+  try {
+    const url = `${BASE_URL}movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+
+export function displayFavMovie(data) {
+  const movieContent = document.querySelector(".main-movie");
+  let html = "";
+  const { title, poster_path,overview,backdrop_path, release_date, vote_average, id } = data;
+  const poster = poster_path
+    ? `${BASE_IMG_URL}${poster_path}`
+    : DEFAULT_IMG_URL;
+
+  html += `
+    <div class="col-4">
+            <a href="" class="movie-posterimg">
+              <img class="img-fluid "
+                src="${poster}" alt="brand" />
+            </a>
+          </div>
+          <div class="col-8">
+            <a href="#" class="movie-title my-2">
+              <h1>
+              ${title}
+              </h1>
+            </a>
+            <p class="my-2">
+              12/17/2021 (US)
+              Action, Adventure, Science Fiction
+              2h 28m
+            </p>
+            <div class="rating mt-5">
+              <div class="circle-progressbar">
+                <div role="progressbar" style="--value: ${vote_average * 10}">
+                </div>
+              </div>
+              <h6 class="pb-4 mx-2">User Score</h6>
+              <ul>
+                <li>
+                  <a href="#" title="Add to list">
+                    <i class="fa-solid fa-list"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Mark as favorite">
+                    <i class="fa-solid fa-heart"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Mark as favorite">
+                    <i class="fa-solid fa-clipboard-list"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#" title="Rate It">
+                    <i class="fa-solid fa-star"></i>
+                  </a>
+                </li>
+              </ul>
+
+            </div>
+            <div class="movie-overview my-2">
+              <h3 class="py-2">
+                Overview
+              </h3>
+              <p> ${overview}
+              </p>
+            </div>
+            <div class="row">
+              <div class="col-4">
+                <a href="" class="movie-character">
+                  steve ditko
+                </a>
+                <p>
+                  characters
+                </p>
+              </div>
+              <div class="col-4">
+                <a href="" class="movie-character ">
+                  stan lee
+                </a>
+                <p>
+                  characters
+              </div>
+              <div class="col-4">
+                <a href="" class="movie-character">
+                  jon watts
+                </a>
+                <p>
+                  directors
+              </div>
+              <div class="col-4">
+                <a href="" class="movie-character">
+                  erik sommers
+                </a>
+                <p>
+                  writer
+              </div>
+              <div class="col-4">
+                <a href="" class="movie-character">
+                  chris mckenna
+                </a>
+                <p>
+                  writer
+              </div>
+            </div>
+          </div>
+    `;
+
+  movieContent.innerHTML = html;
+}
+
--- js/router.js
+++ js/router.js
@@ -1,7 +1,26 @@
 import * as home from "./home.js";
 import * as card from "./card.js";
 import { displayMovie, getMovie } from "./movie.js";
+import {displayFavMovie, getFavMovie} from "./favmovies.js";
+import * as favpersons from "./favperson.js";
 let { circleBtn, cardList, body, switchOn } = card;
+
+
+document.addEventListener("DOMContentLoaded", function (e){
+  if (location.pathname === "/favperson.html" || location.pathname === "/"){
+    // console.log(data);
+    favpersons
+    .getFavPerson()
+    .then((data)=>{
+      favpersons.displayFavPerson(data)
+      console.log(data);
+    })
+    .catch((err) =>{
+      console.log(err);
+    })
+  }
+});
+
 
 document.addEventListener("DOMContentLoaded", function (e) {
   // switchOn()
@@ -44,17 +63,49 @@ document.addEventListener("DOMContentLoaded", function (e) {
 import * as topmovies from "./favmovies.js";
 
 document.addEventListener("DOMContentLoaded", function (e) {
+  // if (location.pathname === "/favmovie.html" || location.pathname === "/") {
+  //   topmovies
+  //     topmovies
+  //     .then((data) => {
+  //       topmovies.displayTopMovies(data);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }
+
+  // switchOn()
   if (location.pathname === "/favmovie.html" || location.pathname === "/") {
     topmovies
       .getTopMovies()
       .then((data) => {
         topmovies.displayTopMovies(data);
+        const cardList = document.querySelectorAll(".card");
+        cardList.forEach((card) => {
+          card.addEventListener("click", (e) => {
+            const id = card.dataset.id;
+            history.pushState({ id }, null, `/detailsmovie.html`);
+            location.reload();
+          });
+        });
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  // if (location.pathname === "/person.html" || location.pathname === "/person") {
-  //   console.log("Bu person page");
-  // }
+  
+  if (
+    location.pathname === "/detailsmovie.html" ||
+    location.pathname === "/movie"
+  ) {
+    // let searchParams = new URLSearchParams(location.search);
+    // searchParams.get("id")
+    const id = history.state.id;
+    console.log(history.state);
+    getFavMovie(id).then((data) => {
+      displayFavMovie(data);
+      console.log(data);
+    });
+  }
 });
+

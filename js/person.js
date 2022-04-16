9152a470ffcadb82cc43a34cdf85cb4035375c32
Asror Qayumov <qayumovasror2003@gmail.com>
2022-04-16 14:13:46 +0500

    merged

--- js/person.js
+++ js/person.js
@@ -2,9 +2,9 @@ import configs from "../config.js";
 import moment from "../node_modules/moment/dist/moment.js";
 const { API_KEY, BASE_URL, DEFAULT_IMG_URL, BASE_IMG_URL } = configs;
 
-export async function getMovie(movie_id) {
+export async function getPerson(person_id) {
   try {
-    const url = `${BASE_URL}movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
+    const url = `${BASE_URL}person/${person_id}?api_key=${API_KEY}&language=en-US`;
     const res = await fetch(url);
     const data = await res.json();
     return data;
@@ -15,113 +15,94 @@ export async function getMovie(movie_id) {
 
 
 
-export function displayMovie(data) {
-  const movieContent = document.querySelector(".main-movie");
+export function displayPerson(data) {
+  const movieContent = document.querySelector(".main-person");
   let html = "";
-  const { title, poster_path,overview, genres, runtime, release_date, vote_average,tagline, id } = data;
-  const poster = poster_path
-    ? `${BASE_IMG_URL}${poster_path}`
+  const { biography, profile_path, name, place_of_birth, birthday, gender, } = data;
+  const poster = profile_path
+    ? `${BASE_IMG_URL}${profile_path}`
     : DEFAULT_IMG_URL;
-
+ const gen = gender =  1 ? 'Female': 'Male'
   html += `
-    <div class="col-4">
-            <a href="" class="movie-posterimg">
-              <img class="img-fluid "
-                src="${poster}" alt="brand" />
-            </a>
-          </div>
-          <div class="col-8">
-            <a href="#" class="movie-title my-2">
-              <h1>
-               ${title} (${release_date.split("-")[0]})
-              </h1>
-            </a>
-            <p class="my-2">
-             ${moment(release_date).format("L")} 
-              ${genres.map((genre) => genre.name).join(", ")}
-             ${Math.floor(runtime / 60)}h ${runtime % 60}m
-            </p>
-            <div class="rating mt-5">
-              <div class="circle-progressbar">
-                <div role="progressbar" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100" style="--value: ${vote_average * 10}">
-                </div>
-              </div>
-              <h6 class="pb-4 mx-2">User Score</h6>
-              <ul>
-                <li>
-                  <a href="#" title="Add to list">
-                    <i class="fa-solid fa-list"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" title="Mark as favorite">
-                    <i class="fa-solid fa-heart"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" title="Mark as favorite">
-                    <i class="fa-solid fa-clipboard-list"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#" title="Rate It">
-                    <i class="fa-solid fa-star"></i>
-                  </a>
-                </li>
-              </ul>
+  <div class="col-sm-7 col-md-4">
+  <a href="" class="movie-posterimg">
+    <img
+      class="img-fluid"
+      src="${poster}"
+      alt="brand"
+    />
+  </a>
+  <ul class="person-social-links my-4">
+    <li>
+      <a href="" title="Visit Facebook">
+        <i class="fa-brands fa-facebook-square"></i>
+      </a>
+    </li>
+    <li>
+      <a href="" title="Visit Insgagram">
+          <i class="fa-brands fa-instagram"></i>
+      </a>
+    </li>
+  </ul>
 
-            </div>
-            <div class="movie-overview my-2">
-            <p class="tagline-text">
-            ${tagline}
-            
-            </p>
-              <h3 class="py-2">
-                Overview
-              </h3>
-              <p class="text">
-                ${overview}
-              </p>
-            </div>
-            <div class="row">
-              <div class="col-4">
-                <a href="" class="movie-character">
-                  steve ditko
-                </a>
-                <p>
-                  characters
-                </p>
-              </div>
-              <div class="col-4">
-                <a href="" class="movie-character ">
-                  stan lee
-                </a>
-                <p>
-                  characters
-              </div>
-              <div class="col-4">
-                <a href="" class="movie-character">
-                  jon watts
-                </a>
-                <p>
-                  directors
-              </div>
-              <div class="col-4">
-                <a href="" class="movie-character">
-                  erik sommers
-                </a>
-                <p>
-                  writer
-              </div>
-              <div class="col-4">
-                <a href="" class="movie-character">
-                  chris mckenna
-                </a>
-                <p>
-                  writer
-              </div>
-            </div>
-          </div>
+  <h3>Personal Info</h3>
+  <p class="py-1">
+    <strong class="fw-bold">Known For</strong> <br />
+    Acting
+  </p>
+  <p class="py-1">
+    <strong class="fw-bold"> Known Credits</strong> <br />
+    128
+  </p>
+  <p class="py-1">
+    <strong class="fw-bold">Gender</strong> <br />
+   ${gen}
+  </p>
+  <p class="py-1">
+    <strong class="fw-bold">Birthday</strong> <br />
+   ${birthday} 
+  </p>
+  <p class="py-1">
+    <strong class="fw-bold">Place of Birth</strong> <br />
+    ${place_of_birth}
+  </p>
+  <p class="py-1">
+    <strong class="fw-bold">Also Known As</strong> <br />
+    Ethan Green Hawke
+  </p>
+</div>
+<div class="col-sm-5 col-md-8 py-1">
+  <h1>
+    <strong class="fw-bold">${name}</strong>
+  </h1>
+  <h5 class="py-4">
+    <strong class="fw-bold">Biography</strong>
+  </h5>
+  <p class="text-dark fw-normal">
+    ${biography}
+  </p>
+  <h5 class="py-4">
+      <strong class="fw-bold">Known For</strong>
+  </h5>
+  <div class="card-person mt-1">
+ <div class="row overflow-scroll">
+  <div class="col">
+      <div class="card">
+        <a href="" class="card-img">
+          <img class="card-img-top"
+            src="https://www.themoviedb.org/t/p/w150_and_h225_bestv2/ai40gM7SUaGA6fthvsd87o8IQq4.jpg"
+            alt="Card image cap">
+        </a>
+        <div class="card-body py-3">
+          <a class="card-text text-capitalize">
+              Dead Poets Society
+          </a>
+        </div>
+      </div>
+    </div>      
+ </div>
+  </div>
+</div>
     `;
   movieContent.innerHTML = html;
 }

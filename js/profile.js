8db3d43b36789028e337f53674f2133f2018c94e
Asror Qayumov <qayumovasror2003@gmail.com>
2022-04-26 15:37:43 +0500

    Some bugs are fixed

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,4 @@
-.import configs from "../config.js";
+import configs from "../config.js";
 import moment from "../node_modules/moment/dist/moment.js";
 const { API_KEY, BASE_URL, DEFAULT_IMG_URL, BASE_IMG_URL, SESSION_ID } = configs;
 

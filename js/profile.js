88010ed4e0fee80814c5cfd32d55fc7764358cfd
Orziqul <orziuktam@mail.ru>
2022-05-03 14:37:43 +0500

    some changes

--- js/profile.js
+++ js/profile.js
@@ -59,58 +59,116 @@ export async function getDetailAccount() {
     const favMovieAccount = document.querySelector(".favMovie-account");
     let html = "";
     results.forEach((DetailAccount) => {
-      const { original_title, poster_path, release_date, id } = DetailAccount;
+      const { original_title, poster_path, release_date, id, overview, vote_average } = DetailAccount;
       const poster = poster_path
         ? `${BASE_IMG_URL}${poster_path}`
         : DEFAULT_IMG_URL;
   
       html += `
-      <div class="col">
-        <card data-id=${id} class="card">
-        <a href="#" class="card-img-btn">
-          <img
-            class="card-img img-fluid"
-            src="${poster}"
-            alt="something movie"
-          />
-        </a>
-        <div class="card-img-overlay">
-          <a class="threedot-btn">
-            <i class="fa-solid fa-ellipsis"></i>
-          </a>
-        </div>
-        <div class="card-body">
-          <div class="card-click">
-            <ul>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-list"></i>
-                  Add to list</a
-                >
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-heart"></i>
-                  Favourite</a
-                >
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-clipboard-list"></i>
-                  Watchlist
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-star"></i>
-                  Your rating</a
-                >
-              </li>
-            </ul>
-
-          
-        </card>
-      </div>
+      <div class="row" style="display: flex; flex-direction: row;">
+            <div class="col">
+              <div class="container">
+                <div class="row  favMovie-account">
+                  <div class="col" style="display: flex; margin-top: 20px;">
+                    <card data-id=${id} class="card">
+                      <a href="#" class="card-img-btn">
+                        <img
+                          class="card-img img-fluid"
+                          src="${poster}"
+                          alt="something movie"
+                        />
+                      </a>
+                      <div class="card-img-overlay">
+                        <a class="threedot-btn">
+                          <i class="fa-solid fa-ellipsis"></i>
+                        </a>
+                      </div>
+                      <div class="card-body">
+                        <div class="card-click">
+                          <ul>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-list"></i>
+                                Add to list</a
+                              >
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-heart"></i>
+                                Favourite</a
+                              >
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-clipboard-list"></i>
+                                Watchlist
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-star"></i>
+                                Your rating</a
+                              >
+                            </li>
+                          </ul>
+                        </div>
+                        <div class="circle-progressbar">
+                          <div
+                            role="progressbar"
+                            aria-valuenow="88"
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                            style="--value: ${vote_average * 10}"
+                          ></div>
+                        </div>
+                      </div>
+                    </card>
+                    <div class="col" style="padding-left: 20px;">
+                      <div class="row">
+                        <div class="col">
+                          <h4><a style="text-decoration: none;" href="#">${original_title}</a></h4>
+                          <p class="text-muted">${release_date}</p>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col">
+                          <p class="text-muted">${overview}</p>
+                          
+                        </div>
+                        
+                      </div>
+                      <div class="row">
+                        <div class="col ">
+                          <ul style="display: flex; justify-content: flex-start;">
+                            <li style="padding-right: 30px">
+                              <a href="#" title="Add to list">
+                                <i class="fa-solid fa-list"> </i>
+                              </a>
+                            </li>
+                            <li style="padding-right: 30px">
+                              <a href="#" title="Mark as favorite">
+                                <i class="fa-solid fa-heart"></i>
+                              </a>
+                            </li>
+                            <li style="padding-right: 30px">
+                              <a href="#" title="Mark as favorite">
+                                <i class="fa-solid fa-clipboard-list"> </i>
+                              </a>
+                            </li>
+                            <li >
+                              <a href="#" title="Rate It">
+                                <i class="fa-solid fa-star"></i>
+                              </a>
+                            </li>
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+            </div>
+          </div>
+      
       `;
       favMovieAccount.innerHTML = html;
     });
@@ -118,7 +176,7 @@ export async function getDetailAccount() {
 
   export async function getRating(page = 1) {
     try {
-      const url = `${BASE_URL}account/{account_id}/watchlist/movies?api_key=${API_KEY}&session_id=${SESSION_ID}&language=en-US&sort_by=created_at.asc&${page}`;
+      const url = `${BASE_URL}account/{account_id}/rated/movies?api_key=${API_KEY}&session_id=${SESSION_ID}&language=en-US&sort_by=created_at.asc&${page}`;
       
       const res = await fetch(url);
       const data = await res.json();
@@ -132,59 +190,117 @@ export async function getDetailAccount() {
     const rated = document.querySelector(".Rated");
     let html = "";
     results.forEach((DetailAccount) => {
-      const { original_title, poster_path, release_date, id } = DetailAccount;
+      const { original_title, poster_path, release_date, id,overview, vote_average } = DetailAccount;
       const poster = poster_path
         ? `${BASE_IMG_URL}${poster_path}`
         : DEFAULT_IMG_URL;
 
         html += `
-        <div class="col">
-        <card data-id=${id} class="card">
-        <a href="#" class="card-img-btn">
-          <img
-            class="card-img img-fluid"
-            src="${poster}"
-            alt="something movie"
-          />
-        </a>
-        <div class="card-img-overlay">
-          <a class="threedot-btn">
-            <i class="fa-solid fa-ellipsis"></i>
-          </a>
+        <div class="row" style="display: flex; flex-direction: row;">
+            <div class="col">
+              <div class="container">
+                <div class="row  Rated">
+                  <div class="col" style="display: flex; margin-top: 20px;">
+                    <card data-id=${id} class="card">
+                      <a href="#" class="card-img-btn">
+                        <img
+                          class="card-img img-fluid"
+                          src="${poster}"
+                          alt="something movie"
+                        />
+                      </a>
+                      <div class="card-img-overlay">
+                        <a class="threedot-btn">
+                          <i class="fa-solid fa-ellipsis"></i>
+                        </a>
+                      </div>
+                      <div class="card-body">
+                        <div class="card-click">
+                          <ul>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-list"></i>
+                                Add to list</a
+                              >
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-heart"></i>
+                                Favourite</a
+                              >
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-clipboard-list"></i>
+                                Watchlist
+                              </a>
+                            </li>
+                            <li>
+                              <a href="#">
+                                <i class="fa-solid fa-star"></i>
+                                Your rating</a
+                              >
+                            </li>
+                          </ul>
+                        </div>
+                        <div class="circle-progressbar">
+                          <div
+                            role="progressbar"
+                            aria-valuenow="88"
+                            aria-valuemin="0"
+                            aria-valuemax="100"
+                            style="--value: ${vote_average * 10}"
+                          ></div>
+                        </div>
+                      </div>
+                    </card>
+                    <div class="col" style="padding-left: 20px;">
+                      <div class="row">
+                        <div class="col">
+                          <h4><a style="text-decoration: none;" href="#">${original_title}</a></h4>
+                          <p class="text-muted">${release_date}</p>
+                        </div>
+                      </div>
+                      <div class="row">
+                        <div class="col">
+                          <p class="text-muted">${overview}</p>
+                          
+                        </div>
+                        
+                      </div>
+                      <div class="row">
+                        <div class="col">
+                        <ul style="display: flex; justify-content: flex-start;">
+                        <li style="padding-right: 30px">
+                          <a href="#" title="Add to list">
+                            <i class="fa-solid fa-list">  </i>
+                          </a>
+                        </li>
+                        <li style="padding-right: 30px">
+                          <a href="#" title="Mark as favorite">
+                            <i class="fa-solid fa-heart"></i>
+                          </a>
+                        </li>
+                        <li style="padding-right: 30px">
+                          <a href="#" title="Mark as favorite">
+                            <i class="fa-solid fa-clipboard-list"> </i>
+                          </a>
+                        </li>
+                        <li >
+                          <a href="#" title="Rate It">
+                            <i class="fa-solid fa-star"> </i>
+                          </a>
+                        </li>
+                      </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  
+            </div>
+          </div>
         </div>
-        <div class="card-body">
-          <div class="card-click">
-            <ul>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-list"></i>
-                  Add to list</a
-                >
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-heart"></i>
-                  Favourite</a
-                >
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-clipboard-list"></i>
-                  Watchlist
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <i class="fa-solid fa-star"></i>
-                  Your rating</a
-                >
-              </li>
-            </ul>
-
-          
-            <h4> ${original_title}</h4>
-        </card>
-      </div>
+          </div>
         `;
         rated.innerHTML = html;
     });
